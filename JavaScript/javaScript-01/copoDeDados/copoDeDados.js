438ea2d3775f7a08e41c331c59dd496d34935a29
Cauecomiran <clsc.caue@gmail.com>
2026-05-21 22:04:41 -0300

    dino

--- JavaScript/javaScript-01/copoDeDados/copoDeDados.js
+++ JavaScript/javaScript-01/copoDeDados/copoDeDados.js
@@ -1,39 +1,53 @@
-let numeroAleatorio, resultado, resultadoTotal
+let numeroAleatorio, resultadoTotal = 0
 
 function d2(){
-    numeroAleatorio = Math.random()
 
-   let dd2 = numeroAleatorio * 2
-    
-    resultado = Math.ceil(dd2)
+    let dd2
 
-  document.getElementById('resultado').innerHTML = "Resultado do D2: " + resultado
+numeroAleatorio = Math.random()* 2
+dd2 = Math.ceil(numeroAleatorio)
+    document.getElementById('resultado').innerHTML = "Resultado do D2: " + dd2
+
+/////////
+
+    resultadoTotal = resultadoTotal + dd2
+    document.getElementById('resultadoTotal').innerHTML = "Resultado Total: " + resultadoTotal
+
+///////////
+ 
+    document.getElementById('rolagens').innerHTML += "Rolagem de d2: " + dd2 + '<br>'
+
+
 }
 
 function  d4(){
-let dd4, resultado
+let dd4
 
 numeroAleatorio = Math.random()*4
-
 dd4 = Math.ceil(numeroAleatorio)
 
-resultado = dd4
-
 document.getElementById('resultado').innerHTML = ('Resultado do D4: ' + dd4)
-
+//======
+resultadoTotal = resultadoTotal + dd4
+document.getElementById('resultadoTotal').innerHTML = "Resultado Total: " + resultadoTotal
+//==========
+document.getElementById('rolagens').innerHTML += "Rolagem de d4: " + dd4 + '<br>'
 }
 
 function d6(){
 
 let dd6
 
-    numeroAleatorio = Math.random()*4
+    numeroAleatorio = Math.random()*6
 
     dd6 = Math.ceil(numeroAleatorio)
 
     document.getElementById('resultado').innerHTML = "Resultado do D6: " + dd6
 
+    resultadoTotal = resultadoTotal + dd6
+    document.getElementById('resultadoTotal').innerHTML = "Resultado Total: " + resultadoTotal
 
+    document.getElementById('rolagens').innerHTML += "Rolagem de d6: " + dd6 + '<br>'
 }
 
 function d8(){
@@ -45,6 +59,11 @@ resultado = dd8
 
 document.getElementById('resultado').innerHTML = "Resultado do D8: " + dd8
 
+resultadoTotal = resultadoTotal + dd8
+document.getElementById('resultadoTotal').innerHTML = "Resultado Total: " + resultadoTotal
+
+document.getElementById('rolagens').innerHTML += "Rolagem de d8: " + dd8 + '<br>'
+
 
 }
 
@@ -55,9 +74,12 @@ numeroAleatorio = Math.random()*10
 
     dd10 = Math.ceil(numeroAleatorio)
 
-    resultado = dd10
-
 document.getElementById('resultado').innerHTML = "Resultado do D10: " + dd10
+
+resultadoTotal = resultadoTotal + dd10
+document.getElementById('resultadoTotal').innerHTML = "Resultado Total: " + resultadoTotal
+
+document.getElementById('rolagens').innerHTML += "Rolagem de d10: " + dd10 + '<br>'
 
 
 }
@@ -71,14 +93,45 @@ numeroAleatorio = Math.random()*12
 dd12 = Math.ceil(numeroAleatorio)
 
 document.getElementById('resultado').innerHTML = "Resultado do D12: " + dd12
+
+resultadoTotal = resultadoTotal + dd12
+document.getElementById('resultadoTotal').innerHTML = "Resultado Total: " + resultadoTotal
+
+document.getElementById('rolagens').innerHTML += "Rolagem de d12: " + dd12 + '<br>'
+
 }
 
 function d20(){
 
+    let dd20
+
+numeroAleatorio = Math.random()*20
+
+dd20 = Math.ceil(numeroAleatorio)
+
+document.getElementById('resultado').innerHTML = "Resultado do D20: " + dd20
+
+resultadoTotal = resultadoTotal + dd20
+document.getElementById('resultadoTotal').innerHTML = "Resultado Total: " + resultadoTotal
+
+document.getElementById('rolagens').innerHTML += "Rolagem de d20: " + dd20 + '<br>'
 
 
 }
 
 function d100(){
+
+    let dd100
+
+numeroAleatorio = Math.random()*100
+dd100 = Math.ceil(numeroAleatorio)
+
+
+document.getElementById('resultado').innerHTML = "Resultado do D100: " + dd100
+ 
+resultadoTotal = resultadoTotal + dd100
+document.getElementById('resultadoTotal').innerHTML = "Resultado Total: " + resultadoTotal
+
+document.getElementById('rolagens').innerHTML += "Rolagem de d100: " + dd100 + '<br>'
 }
 
