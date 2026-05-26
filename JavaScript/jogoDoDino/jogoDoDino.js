35683558fe19a5c0ba2b98fc1a5075ddc431bc14
Cauecomiran <clsc.caue@gmail.com>
2026-05-26 20:02:07 -0300

    oi

--- JavaScript/jogoDoDino/jogoDoDino.js
+++ JavaScript/jogoDoDino/jogoDoDino.js
@@ -1,20 +1,35 @@
-let lucro, valorAposta, premio
+let lucro = 0, valorAposta = 0, premio = 0
 
 function aposta(dino){
     let numeroSorteado
         numeroSorteado = Math.ceil(Math.random()*10)
 
-        valorApostaposta = Number(document.getElementById("inputAposta").value)
+        valorAposta = Number(document.getElementById("inputAposta").value)
         premio = valorAposta * 2
 
-    if(numeroSorteado == dino && valorDeAposta <= lucro*0.2 )
+    
+    if(numeroSorteado == dino && premio <= lucro*0.2 )
         {
             document.getElementById("dinoSorteado").innerHTML = "Você ganhou!"
+
+            lucro = lucro - premio
+            alert(lucro)
+
+            //====
+            if(dino == 1){
+                document.getElementById("dinoEscolhido").innerHTML = "Seu dino = T-Rex"
+            }
+
+
+
         } else {
             document.getElementById("dinoSorteado").innerHTML = "Você perdeu!"
+        
+            lucro += valorAposta
+            alert(lucro)
         }
 
     
-
+  
 
 }
