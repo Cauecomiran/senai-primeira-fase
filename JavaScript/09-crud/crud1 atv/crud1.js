9d26ac744313100e2023990084817d3cfbe624a4
Cauecomiran <clsc.caue@gmail.com>
2026-07-17 21:56:45 -0300

    oi

--- JavaScript/09-crud/crud1 atv/crud1.js
+++ JavaScript/09-crud/crud1 atv/crud1.js
@@ -8,10 +8,35 @@
 
     
 
-    const dinos = []
+    const dinos = [
+        {
+          id: 1234,
+          nome: "T-Rex",
+          altura: 0.25,
+          custo: 49.90,
+          cor: "Verde Musgo"
+        },
+        {
+          id: 5678,
+          nome: "Triceratops",
+          altura: 0.15,
+          custo: 35.00,
+          cor: "Azul"
+        },
+        {
+          id: 91011,
+          nome: "Velociraptor",
+          altura: 0.20,
+          custo: 59.90,
+          cor: "Marrom Cinzento"
+        }
+    ];
+
     const mostrar = document.getElementById("mostrar-dinos")
 
     function adicionar(){
+
+        
 
         const novoDino = {
 
@@ -30,27 +55,32 @@
 
         document.getElementById("input-nome").focus()
 
-
        dinos.push(novoDino)
 
     }
     
     function mostrarDinos(){
-
+        
+        mostrar.innerHTML =""
        dinos.forEach((dino, i) =>{
         
-            const celula = document.createElement("div");
+            const div = document.createElement("div");
 
-            celula.className = "celula";
-            celula.id = "item" + i;
-            celula.innerHTML = `<span> O nome do dino  ${dino.nome} </span>`
+            div.className = "celula";
+            div.id = "item" + i;
+            div.innerHTML = `<span> 
+            <h2>${dino.nome}</h2>
+            <p>altura: ${dino.altura} </p>
+            <p>Cor: ${dino.cor}</p>
+            <p>Custo: ${dino.custo} </p> </span>`
+
 
             
 
-        mostrar.appendChild(celula)
+        mostrar.appendChild(div)
 
-        //console.log(dinos[0].nome)
-        console.log(dinos)
+        
+        
         
     })}
         
